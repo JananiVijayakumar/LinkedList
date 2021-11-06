966371807631133ce4bbef0fc07ea273771c9c42
Janani <vsvprema@gmail.com>
2021-11-06 10:31:15 +0530

    Added: Simple Linked list

---
+++ src/com/bridgelabz/SimpleLinkedList.java
@@ -0,0 +1,85 @@
+package com.bridgelabz;
+
+/**
+ * Author : jananiVijayakumar
+ * Date : 01/11/2021
+ * Desc : Simple Linked list of 56,30 and 70
+ */
+
+public class SimpleLinkedList {
+    public static class Node {
+        private int data;
+        private Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public int size;
+    //initialize head and tail
+    private Node head = null;
+    private Node tail = null;
+
+    //Create a new node to add the data in list and Checks if the list is empty
+    public void createNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        tail = newNode;
+        size++;
+    }
+
+    //display all the nodes
+    public void display() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+            return;
+        }
+        System.out.println("Simple Linked List :");
+        while (temp != null) {
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    //Add nodes at the Beginning of the list
+    public void addNodeAtBeginning(int data) {
+        Node node = new Node(data);
+        if (head == null) {
+            this.head = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
+    }
+
+    //Add nodes at the end of the list
+    public void addNodeAtEnd(int data) {
+        Node node = new Node(data);
+        if (head == null) {
+            this.head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+
+    public static void main(String[] args) {
+        SimpleLinkedList sl = new SimpleLinkedList();
+        //Add nodes to the list
+        sl.addNodeAtEnd(56);
+        sl.addNodeAtEnd(30);
+        sl.addNodeAtEnd(40);
+        sl.display();
+    }
+}
